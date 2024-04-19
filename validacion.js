f53184a280c73546f0682bd6f0b44ffa7e7b7310
devtapias <mcdavidtapias@hotmail.com>
2024-04-19 14:52:57 -0500

    se modifica el readme

--- validacion.js
+++ validacion.js
@@ -1,1 +1,15 @@
-//Haz tú validación en javascript acá
+onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btn-volver-arriba").style.display = "block";
+  } else {
+    document.getElementById("btn-volver-arriba").style.display = "none";
+  }
+}
+
+// Función para volver al inicio al hacer clic en el botón
+document.getElementById("btn-volver-arriba").onclick = function() {
+  document.body.scrollTop = 0; // Para navegadores que no son compatibles con document.documentElement.scrollTop
+  document.documentElement.scrollTop = 0; // Para navegadores compatibles
+}
